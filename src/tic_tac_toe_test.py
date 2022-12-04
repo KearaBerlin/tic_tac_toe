40cc1167a5a0e1d32f883fb86b154fb450db5f39
Keara Berlin <kearaberlin@gmail.com>
2022-12-04 14:28:44 -0600

    fix range index and update comments

--- src/tic_tac_toe_test.py
+++ src/tic_tac_toe_test.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 ###
 # Test calling the method in tic_tac_toe_robot.py
-# TODO this isn't implemented yet.
 ###
 
 from tic_tac_toe_robot import TicTacToeRobot as Robot
 
 if __name__ == "__main__":
     robot = Robot()
-    for i in range(8):
+    for i in range(9):
     	robot.mark_square(i)
