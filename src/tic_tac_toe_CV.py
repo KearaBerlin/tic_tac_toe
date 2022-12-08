9945aca212a542c8ee7f3f0a9557a78400b6adda
Amarachi <amarachi.nzeukwu@gmail.com>
2022-12-07 23:05:30 -0600

    Includes preprocess method (rotate n crop)

--- src/tic_tac_toe_CV.py
+++ src/tic_tac_toe_CV.py
@@ -6,6 +6,7 @@ Created on Sun Oct 23 15:42:50 2022
 """
 
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 def get_image():
 	
@@ -27,21 +28,24 @@ def get_image():
     return frame
 
 def create_game_state(img):
-    #img = cv.imread("Board space2.png", 1)
-#cv.imshow('img', img)
-
-    height, width, c = img.shape
+    #img = cv.imread("test_images/Board space4.png", 1)
+    #cv.imshow('img', img)
+    
+    img = preprocess_image(img)
+    
+    height, width, c = img.shape 
     h = height // 3
     w = width // 3
 
     board = []
     for i in range(3):
         for j in range(3):
-            filename = str(i*3 + j) + ".jpg"
-        #cv.imwrite(filename, img[i*h:(i+1)*h,j*h:(j+1)*h,:])
-            if detect(img[i*h:(i+1)*h,j*h:(j+1)*h,:]) == -1:
+            #filename = str(i*3 + j) + ".jpg"
+            #cv.imwrite(filename, img[i*h:(i+1)*h,j*h:(j+1)*h,:])
+            detect_pred = detect(img[i*h:(i+1)*h,j*w:(j+1)*h,:])
+            if detect_pred == -1:
                 letter = 'O'
-            elif detect(img[i*h:(i+1)*h,j*h:(j+1)*h,:]) == 1:
+            elif detect_pred == 1:
                 letter = 'X'
             else:
                 letter = ""
@@ -58,20 +62,31 @@ def detect(img):
         for c in range(width):
             blue = int(img[r, c, 0])
             red = int(img[r, c, 2])
-            if blue - red > 50:
+            if blue - red > 55:
                 b_count += 1
-            elif red - blue > 50:
+            elif red - blue > 55:
                 r_count += 1
                 
-    if r_count > b_count:
+    if r_count - b_count > 60:
         return -1
-    elif b_count > r_count:
+    elif b_count - r_count > 50:
         return 1
     else:
         return 0
     
                 
     return []
+
+def preprocess_image(img):
+    height, width, c = img.shape
+    
+    center = (300, 350)
+    m = cv.getRotationMatrix2D(center=center, angle=180, scale=1)
+    img = cv.warpAffine(src=img, M=m, dsize=(width, height))
+    cropped_img = img[355:455,195:300,:]
+    plt.imshow(cropped_img[:,:,::-1])
+    return cropped_img
+    
         
 print(create_game_state(get_image()))
 
