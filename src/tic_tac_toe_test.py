98d4bba405347cfc81014fe5e712f2f87b18abd2
Keara Berlin <kearaberlin@gmail.com>
2022-12-07 19:07:11 -0600

    enable cmd line argument to say which square to move to

--- src/tic_tac_toe_test.py
+++ src/tic_tac_toe_test.py
@@ -4,8 +4,14 @@
 ###
 
 from tic_tac_toe_robot import TicTacToeRobot as Robot
+import sys
 
 if __name__ == "__main__":
     robot = Robot()
-    for i in range(9):
-    	robot.mark_square(i)
+    if len(sys.argv) >= 2:
+        i = int(sys.argv[1])
+        if 0 <= i and i <= 8:
+            robot.mark_square(i)
+    else:
+        for i in range(9):
+    	    robot.mark_square(i)
